be9f3c6769504dde74fdba164fdd23df43e9ebde
YiWei <ywei390@gmail.com>
2024-10-24 10:58:42 +0800

    feat:  enable configuration for gleif enricher setup

--- src/ExternalSearch.Providers.Gleif/GleifExternalSearchJobData.cs
+++ src/ExternalSearch.Providers.Gleif/GleifExternalSearchJobData.cs
@@ -7,14 +7,19 @@ namespace CluedIn.ExternalSearch.Providers.Gleif
     {
         public GleifExternalSearchJobData(IDictionary<string, object> configuration)
         {
-           
+            AcceptedEntityType = GetValue(configuration, nameof(AcceptedEntityType), default(string));
+            LeiVocabularyKey = GetValue(configuration, nameof(LeiVocabularyKey), default(string));
         }
 
         public IDictionary<string, object> ToDictionary()
         {
             return new Dictionary<string, object> {
-                
+                { nameof(AcceptedEntityType), AcceptedEntityType },
+                { nameof(LeiVocabularyKey), LeiVocabularyKey },
             };
         }
+
+        public string AcceptedEntityType { get; set; }
+        public string LeiVocabularyKey { get; set; }
     }
 }
