09f8d4ec3f95d963f464c4d840cdf876e9275c4c
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2024-11-25 14:04:32 +0800

    feat: Add guidance note to enricher configuration page (#20)

--- src/ExternalSearch.Providers.Gleif/Constants.cs
+++ src/ExternalSearch.Providers.Gleif/Constants.cs
@@ -10,7 +10,31 @@ namespace CluedIn.ExternalSearch.Providers.Gleif
         public const string ComponentName = "Gleif";
         public const string ProviderName = "Gleif";
         public static readonly Guid ProviderId = Guid.Parse("6d47d335-2bf3-4249-88c4-0f08d322c24c");
-
+        public const string Instruction = """
+            [
+              {
+                "type": "bulleted-list",
+                "children": [
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the entity type to specify the golden records you want to enrich. Only golden records belonging to that entity type will be enriched."
+                      }
+                    ]
+                  },
+                  {
+                    "type": "list-item",
+                    "children": [
+                      {
+                        "text": "Add the vocabulary keys to provide the input for the enricher to search for additional information. For example, if you provide the website vocabulary key for the Web enricher, it will use specific websites to look for information about companies. In some cases, vocabulary keys are not required. If you don't add them, the enricher will use default vocabulary keys."
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+            """;
         public static string About { get; set; } = "Gleif is an enricher which provides information using the Legal Entity Identifier (LEI) of an organization";
         public static string Icon { get; set; } = "Resources.gleif.png";
         public static string Domain { get; set; } = "https://www.gleif.org/en";
@@ -58,7 +82,10 @@ namespace CluedIn.ExternalSearch.Providers.Gleif
             //}
         };
 
-        public static Guide Guide { get; set; } = null;
+        public static Guide Guide { get; set; } = new Guide
+        {
+            Instructions = Instruction
+        };
         public static IntegrationType IntegrationType { get; set; } = IntegrationType.Enrichment;
     }
 }
