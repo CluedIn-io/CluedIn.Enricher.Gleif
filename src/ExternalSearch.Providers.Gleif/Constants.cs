8a1e866cca76f71bd0567ad0bc5020b7473527b0
Yi-Wei <ywei390@gmail.com>
2025-02-27 11:05:14 +0800

    feat: Update terminology

--- src/ExternalSearch.Providers.Gleif/Constants.cs
+++ src/ExternalSearch.Providers.Gleif/Constants.cs
@@ -26,7 +26,7 @@ namespace CluedIn.ExternalSearch.Providers.Gleif
                     "type": "list-item",
                     "children": [
                       {
-                        "text": "Add the entity type to specify the golden records you want to enrich. Only golden records belonging to that entity type will be enriched."
+                        "text": "Add the business domain to specify the golden records you want to enrich. Only golden records belonging to that business domain will be enriched."
                       }
                     ]
                   },
@@ -52,11 +52,11 @@ namespace CluedIn.ExternalSearch.Providers.Gleif
             {
                 new()
                 {
-                    DisplayName = "Accepted Entity Type",
+                    DisplayName = "Accepted Business Domain",
                     Type = "entityTypeSelector",
                     IsRequired = true,
                     Name = KeyName.AcceptedEntityType,
-                    Help = "The entity type that defines the golden records you want to enrich (e.g., /Organization)."
+                    Help = "The business domain that defines the golden records you want to enrich (e.g., /Organization)."
                 },
                 new()
                 {
