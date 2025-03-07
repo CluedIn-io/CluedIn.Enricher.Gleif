b09285a9aba4027f3b240fcd1a48cdf3fdf918c7
Yi-Wei <ywei390@gmail.com>
2025-03-07 18:02:49 +0800

    feat: Supports enricherv2

--- src/ExternalSearch.Providers.Gleif.Provider/GleifSearchProviderProvider.cs
+++ src/ExternalSearch.Providers.Gleif.Provider/GleifSearchProviderProvider.cs
@@ -10,6 +10,7 @@ using CluedIn.Core.Providers;
 using CluedIn.Core.Webhooks;
 using CluedIn.ExternalSearch;
 using CluedIn.ExternalSearch.Providers.Gleif;
+using CluedIn.ExternalSearch.Providers.Gleif.Vocabularies;
 using CluedIn.Providers.Models;
 using Constants = CluedIn.ExternalSearch.Providers.Gleif.Constants;
 
@@ -120,5 +121,16 @@ namespace CluedIn.Provider.Gleif
         public IEnumerable<Control> Properties { get; } = Constants.Properties;
         public Guide Guide { get; } = Constants.Guide;
         public new IntegrationType Type { get; } = Constants.IntegrationType;
+        public bool SupportsEnricherV2 => true;
+        public Dictionary<string, object> ExtraInfo { get; } = new()
+        {
+            { "autoMap", true },
+            { "origin", Constants.ProviderName.ToCamelCase() },
+            { "originField", string.Empty },
+            { "nameKeyField", string.Empty },
+            { "vocabKeyPrefix", GleifVocabularies.Organization.KeyPrefix},
+            { "autoSubmission", false },
+            { "dataSourceSetId", string.Empty },
+        };
     }
 }
