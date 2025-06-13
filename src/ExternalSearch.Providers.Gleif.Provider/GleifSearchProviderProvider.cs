888be9b2a0d240ccec34c8b020089a7c625773f6
Yi-Wei <ywei390@gmail.com>
2025-06-13 13:22:50 +0800

    feat: Supports enricherv2

--- src/ExternalSearch.Providers.Gleif.Provider/GleifSearchProviderProvider.cs
+++ src/ExternalSearch.Providers.Gleif.Provider/GleifSearchProviderProvider.cs
@@ -125,6 +125,10 @@ namespace CluedIn.Provider.Gleif
         public Dictionary<string, object> ExtraInfo { get; } = new()
         {
             { "autoMap", true },
+            { "useEnricherOriginEntityCode", true },
+            { "supportConfidenceScore", false }, // for UI
+            { "minConfidenceScore", 0 }, // for UI
+            { "maxConfidenceScore", 100 }, // for UI
             { "origin", Constants.ProviderName.ToCamelCase() },
             { "originField", string.Empty },
             { "nameKeyField", string.Empty },
